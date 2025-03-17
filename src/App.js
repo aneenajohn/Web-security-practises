2551d9a001f5994066d289c74e8c254a5429df73
aneenajohn <75213145+aneenajohn@users.noreply.github.com>
2025-03-17 04:11:08 +0000

    Add click jack wrapper

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Comments from "./components/xss/stored-xss/Comments";
 import SearchResults from "./components/xss/reflected-xss/searchResults";
 
 export default function App() {
+  console.log("Window chk:", window.self.location, window.top.location);
   return (
     <div className="App">
       <h1>Web Security Practises</h1>
---
+++ src/components/clickjacking/ClickJackWrapper.jsx
@@ -0,0 +1,14 @@
+import { useEffect } from "react";
+
+const ClickjackProtection = () => {
+  useEffect(() => {
+    // ORwindow.top.location !== window.self.location
+    if (window.self !== window.top) {
+      document.body.innerHTML = ""; // Remove content if in an iframe
+    }
+  }, []);
+
+  return <div>Secure Content</div>;
+};
+
+export default ClickjackProtection;
